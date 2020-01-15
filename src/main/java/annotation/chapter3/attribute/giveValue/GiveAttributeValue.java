8e6b9da1a80991e9a1cec1565bf979db3c2f6a65
zk <271226192@qq.com>
2020-01-15 17:48:27 +0800

    delete

--- src/main/java/annotation/chapter3/attribute/giveValue/GiveAttributeValue.java
+++
@@ -1,7 +0,0 @@
-package annotation.chapter3.attribute.giveValue;
-
-import annotation.chapter3.attribute.setupAttr.AttributeDemo;
-
-@AttributeDemo(id = 3, msg = "hello")
-public class GiveAttributeValue {
-}
--- src/main/java/annotation/chapter4/javaDefalut/override/OverrideDemo.java
+++
@@ -1,9 +0,0 @@
-package annotation.chapter4.javaDefalut.override;
-
-public class OverrideDemo {
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
